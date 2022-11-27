cc2c0c1d14a903a360865d7955a37ecd2f61da9f
David uche <103164411+Daviduche03@users.noreply.github.com>
2022-11-27 22:49:18 +0100

    Index.js

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ const li1 = document.querySelector('.li1')
 const li2 = document.querySelector('.li2')
 const li3 = document.querySelector('.li3')
 
-/*btn1.addEventListener('click', ()=>{
+btn1.addEventListener('click', ()=>{
   li1.classList.add('hide')
   li1.classList.remove('show')
   li2.classList.remove('hide')
@@ -32,7 +32,7 @@ btn2.addEventListener('click', ()=>{
   li3.classList.remove('hide')
 })
 
-*/
+
 //menu navigation
 const nav = document.querySelector("#nav")
 const btn_nav = document.querySelector("#btn")
